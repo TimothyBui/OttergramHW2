6de91ded3690c91b09696352b4dbca386c5aa569
TimothyBui <50809539+TimothyBui@users.noreply.github.com>
2021-02-17 21:18:34 -0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,127 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
+var LEFT_BUTTON = 'previous';
+var RIGHT_BUTTON = 'next';
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', imageUrl);
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title')
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+  thumb.addEventListener('click', function (event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+  });
+}
+
+function getThumbnailsArray() {
+  'use strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+}
+
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
+function initializeEvents() {
+  'use strict';
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
+
+  document.getElementById("previous").addEventListener("click", function(event) {
+    event.preventDefault();
+    previousButton();
+  });
+  
+  document.getElementById("next").addEventListener("click", function(event) {
+    event.preventDefault();
+    nextButton();
+  });
+}
+
+initializeEvents();
+
+function getImagesArray() {
+  "use strict";
+  var imgArray = getThumbnailsArray();
+  for (var i = 0; i < imgArray.length; i++) {
+    imgArray[i] = imgArray[i].href;
+  }
+  return imgArray;
+}
+
+function previousButton() {
+  var imgArray = getImagesArray();
+  var thumbArray = getThumbnailsArray();
+  var curr = imgArray.indexOf(document.getElementById("detail-image").src);
+  if (curr == 0) {
+    curr = imgArray.length - 1;
+  } else {
+    curr = curr - 1;
+  }
+  thumbArray[curr].click();
+}
+
+function nextButton() {
+  var imgArray = getImagesArray();
+  var thumbArray = getThumbnailsArray();
+  var curr = imgArray.indexOf(document.getElementById("detail-image").src);
+  if (curr == imgArray.length-1) {
+    curr = 0;
+  } else {
+    curr = curr + 1;
+  }
+  thumbArray[curr].click();
+}
